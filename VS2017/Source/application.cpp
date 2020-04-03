45a4865c5d718aec0fd3d1ad9f8d6999c26a13f9
Noah <noah.horowitz13@gmail.com>
2020-04-02 21:18:10 -0400

    application.cpp integration

--- VS2017/Source/application.cpp
+++ VS2017/Source/application.cpp
@@ -21,8 +21,11 @@
 #include "IndexBuffer.h"
 #include "Shader.h"
 #include "Texture.h"
+
 #include "vendor/imgui/imgui.h"
 #include "vendor/imgui/imgui_impl_glfw_gl3.h"
+
+#include "tests/TestClearColor.h"
 
 using namespace std;
 
@@ -56,18 +59,7 @@ int main(void)
     }
     
     { // used to prevent out of context error at Buffer distruction.
-        float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f, // index 0
-             50.0f, -50.0f, 1.0f, 0.0f, // index 1
-             50.0f,  50.0f, 1.0f, 1.0f, // index 2
-            -50.0f,  50.0f, 0.0f, 1.0f  // index 3
-        };
-    
-        // Index buffer
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
+   
 
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
@@ -76,28 +68,7 @@ int main(void)
         GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
 
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib = IndexBuffer(indices, 6);
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-
-        Texture texture("res/textures/autumn_leaves.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        va.Unbind();
-        vb.UnBind();
-        ib.UnBind();
-        shader.Unbind();
+  
 
         Renderer renderer;
 
@@ -105,51 +76,24 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translationA = glm::vec3(200.0f, 200.0f, 0);
-        glm::vec3 translationB = glm::vec3(400.0f, 200.0f, 0);
-
-        bool show_demo_window = true;
-        bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        test::TestClearColor test;
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             renderer.Clear();
 
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplGlfwGL3_NewFrame();
-
-            shader.Bind();
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-
-            {
-                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
 		}
     }
