93e178929c67e54e9d44eaa7bf5ff850c7e5fd5a
Noah <noah.horowitz13@gmail.com>
2020-04-13 19:40:55 -0400

    Add color to model

--- VS2017/Source/Model.cpp
+++ VS2017/Source/Model.cpp
@@ -9,16 +9,27 @@ Model::Model(const Object& object)
 	m_RelativeRotation = glm::mat4(1.0f);
 	m_RelativeScale = glm::mat4(1.0f);
 	m_ModelMatrix = glm::mat4(1.0f);
+	m_Color = glm::vec4(1.0f);
 }
 
 Model::Model(const Object& object, glm::vec3 translation, float angle, glm::vec3 rotation, glm::vec3 scale)
 	: m_RelativeTranslation(glm::translate(glm::mat4(1.0f), translation)),
 	m_RelativeRotation(glm::rotate(glm::mat4(1.0f), glm::radians(angle), rotation)),
-	m_RelativeScale(glm::scale(glm::mat4(1.0f), scale))
+	m_RelativeScale(glm::scale(glm::mat4(1.0f), scale)), m_Color(glm::vec4(1.0f))
 {
 	m_VAO = &object.getVAO();
 	m_IndexBuffer = &object.getIndexBuffer();
-	m_ModelMatrix = glm::mat4(1.0f);
+	computeModelMatrix();
+}
+
+Model::Model(const Object& object, glm::vec3 translation, float angle, glm::vec3 rotation, glm::vec3 scale, glm::vec4 color)
+	: m_RelativeTranslation(glm::translate(glm::mat4(1.0f), translation)),
+	m_RelativeRotation(glm::rotate(glm::mat4(1.0f), glm::radians(angle), rotation)),
+	m_RelativeScale(glm::scale(glm::mat4(1.0f), scale)), m_Color(color)
+{
+	m_VAO = &object.getVAO();
+	m_IndexBuffer = &object.getIndexBuffer();
+	computeModelMatrix();
 }
 
 Model::~Model()
@@ -32,6 +43,16 @@ void Model::draw(const Shader &shader)
 	Renderer renderer;
 	renderer.Draw(*m_VAO, *m_IndexBuffer, shader);
 }
+
+void Model::draw(Shader& shader, glm::mat4 groupModelMatrix)
+{
+	shader.Bind();
+	shader.SetUniformMat4f("u_Model", groupModelMatrix * m_ModelMatrix);
+	shader.SetUniform4fv("u_Color", m_Color);
+	draw(shader);
+}
+
+
 
 void Model::setTranslation(glm::vec3 translation)
 {
--- VS2017/Source/Model.h
+++ VS2017/Source/Model.h
@@ -7,8 +7,10 @@ class Model {
 public: 
 	Model(const Object &object);
 	Model(const Object& object, glm::vec3 translation, float angle, glm::vec3 rotaiton, glm::vec3 scale);
+	Model(const Object& object, glm::vec3 translation, float angle, glm::vec3 rotaiton, glm::vec3 scale, glm::vec4 color);
 	~Model();
 	virtual void draw(const Shader &shader);
+	virtual void draw(Shader& shader, glm::mat4 groupModelMatrix);
 	void setTranslation(glm::vec3 translation);
 	void setRotation(float angle, glm::vec3 rotation);
 	void setScale(glm::vec3 scale);
@@ -18,6 +20,7 @@ public:
 private:
 	VertexArray *m_VAO;
 	IndexBuffer *m_IndexBuffer;
+	glm::vec4 m_Color;
 	glm::mat4 m_RelativeTranslation;
 	glm::mat4 m_RelativeRotation;
 	glm::mat4 m_RelativeScale;
