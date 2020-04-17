4cf8dffa1d6c04385ac75be551b628e5c48e6d35
daniakalomiris <daniakalomiris@gmail.com>
2020-04-16 23:03:49 -0400

    remove unused color and change shape of Bush3

--- VS2017/Source/Bush1.cpp
+++ VS2017/Source/Bush1.cpp
@@ -10,27 +10,27 @@ Bush1::Bush1(const Shader& shader, Object& cubeObject) : ComplexModel(shader)
 	glm::vec3 scale(0.5f, 0.5f, 0.5f);
 	glm::vec4 limeGreen(0.55f, 0.85f, 0.1f, 1.0f);
 
-	scale = glm::vec3(0.80f, 0.25f, 0.80f);
+	scale = glm::vec3(0.8f, 0.25f, 0.8f);
 	translation = glm::vec3(-0.5f, 0.0f, 0.0f);
 	Model* leaves1 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves1);
 
-	scale = glm::vec3(0.95f, 0.30f, 0.95f);
+	scale = glm::vec3(0.95f, 0.3f, 0.95f);
 	translation = glm::vec3(2.0f, 1.0f, 0.5f);
 	Model* leaves2 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves2);
 
-	scale = glm::vec3(0.95f, 0.30f, 0.95f);
+	scale = glm::vec3(0.95f, 0.3f, 0.95f);
 	translation = glm::vec3(-2.5f, 1.5f, 0.5f);
 	Model* leaves3 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves3);
 
-	scale = glm::vec3(0.55f, 0.30f, 1.25f);
+	scale = glm::vec3(0.55f, 0.3f, 1.25f);
 	translation = glm::vec3(-2.5f, 3.5f, 0.5f);
 	Model* leaves4 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves4);
 
-	scale = glm::vec3(0.55f, 0.30f, 0.5f);
+	scale = glm::vec3(0.55f, 0.3f, 0.5f);
 	translation = glm::vec3(1.5f, 4.0f, 0.5f);
 	Model* leaves5 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves5);
--- VS2017/Source/Bush2.cpp
+++ VS2017/Source/Bush2.cpp
@@ -9,19 +9,18 @@ Bush2::Bush2(const Shader& shader, Object& cubeObject) : ComplexModel(shader)
 	float angle = 0.0f;
 	glm::vec3 scale(0.5f, 0.5f, 0.5f);
 	glm::vec4 limeGreen(0.55f, 0.85f, 0.1f, 1.0f);
-	glm::vec4 brown(0.545f, 0.271f, 0.075f, 1.0f);
 
 	scale = glm::vec3(1.0f, 0.5f, 1.0f);
 	translation = glm::vec3(0.0f, 1.5f, 0.0f);
 	Model* leaves1 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves1);
 
-	scale = glm::vec3(0.45f, 0.15f, 1.40f);
+	scale = glm::vec3(0.45f, 0.15f, 1.4f);
 	translation = glm::vec3(-1.5f, 0.0f, 0.0f);
 	Model* leaves2 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves2);
 
-	scale = glm::vec3(0.18f, 0.1f, 1.40f);
+	scale = glm::vec3(0.2f, 0.1f, 1.4f);
 	translation = glm::vec3(1.0f, 0.0f, 0.0f);
 	Model* leaves3 = new Model(cubeObject, translation, angle, rotation, scale, limeGreen);
 	addModel(leaves3);
